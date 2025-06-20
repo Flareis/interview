a9c580dc8ae7277ac157a025dc8fec6483bc7ec8
FlaReis <flavia.reis@squadra.com.br>
2025-06-20 15:38:25 -0300

    feat: implement service with data encryption

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,21 +1,44 @@
 import { Injectable, NotImplementedException } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Task } from './schemas/tasks.schema';
+import { CreateUserDto } from './application/dto/createUser.dto';
+import { CryptoService } from 'src/helper/crypto.service';
 
 @Injectable()
 export class TasksService {
-  constructor(@InjectModel(Task.name) private repository: Model<Task>) {}
+  constructor() { }
+  private readonly tasks: any[] = [];
 
-  create() {
-    throw new NotImplementedException('Method not implemented');
+  create(createUserDto: CreateUserDto) {
+    const encryptedEmail = CryptoService.encrypt(createUserDto.email);
+
+    const newTask = {
+      id: Date.now().toString(),
+      title: createUserDto.title,
+      description: createUserDto.description,
+      taskDate: createUserDto.taskDate,
+      email: encryptedEmail
+    };
+    this.tasks.push(newTask);
+
+    return newTask;
   }
 
   findAll() {
-    throw new NotImplementedException('Method not implemented');
+    return this.tasks.map(task => {
+      return {
+        ...task,
+        email: CryptoService.decrypt(task.email)
+      };
+    })
   }
 
-  findOne() {
-    throw new NotImplementedException('Method not implemented');
+  findOne(id: string) {
+    const task = this.tasks.find(task => task.id === id);
+    if (!task) {
+      throw new NotImplementedException(`Task with id ${id} not found`);
+    }
+    return {
+      ...task,
+      email: CryptoService.decrypt(task.email)
+    }
   }
 }
