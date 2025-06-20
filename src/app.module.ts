370c986d67baf291ba3a078893d4dca2e0ab92d5
FlaReis <flavia.reis@squadra.com.br>
2025-06-20 15:39:18 -0300

    chore: update app module and bootstrap configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,12 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
     TasksModule,
-    MongooseModule.forRoot('mongodb://test:test@localhost:27017', {
-      dbName: 'tasks',
-    }),
+
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
+      whitelist: false,
       transform: true,
-      forbidNonWhitelisted: true,
+      forbidNonWhitelisted: false,
     }),
   );
   await app.listen(process.env.PORT ?? 3000);
